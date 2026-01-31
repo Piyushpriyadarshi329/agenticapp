e5c8fcdc4cc2214f4df7a6a11cce1985c1f7e81d
piyush <piyush.priyadarshi@antrepriz.com>
2026-01-31 23:15:09 +0530

    Create basic api

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express= require("express")
+const app = express();
+
+
+app.get("/",(req,res)=>{
+    res.send("welcome...")
+})
+
+app.listen(4001,()=>{
+    console.log("server is listen at 4001 port")
+})
